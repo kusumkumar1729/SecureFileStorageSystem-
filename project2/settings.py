6d126a27524c59b6b7747a1927ece9de9378009b
Kusum462 <kusumakumarproject@.com>
2025-09-09 19:53:13 +0530

    Allowed Hosts

--- project2/settings.py
+++ project2/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "securefilestoragesystem-440d.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
